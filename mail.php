0fcf4189ab6c9e8a631a960621f8cdf884ccb506
Andy Woo <andywoo91@hotmail.com>
2014-02-11 23:43:22 -0500

    Added injection detection
    Simple logic to reject emails that show signs of email injection.

--- mail.php
+++ mail.php
@@ -6,6 +6,14 @@ $email=$_POST['email'];
 $website=$_POST['url'];
 $message=$_POST['message'];
 
+// Check if email injection attempt was detected
+if (IsInjected($email))
+{
+    // JavaScript messages to alert user
+    echo "<script type='text/javascript'>alert('Email injection detected!')</script>";
+    echo "<script type='text/javascript'>window.location = 'index.html'</script>";
+}
+
 // Validate input, if not already done elsewhere
 if(empty($name)||empty($email)||empty($message)) 
 {
@@ -26,5 +34,26 @@ else if (!empty($name)&&!empty($email)&&!empty($message))
     // JavaScript thank-you message
 	echo "<script type='text/javascript'>alert('Thanks for your message! I will be in touch.')</script>";
     echo "<script type='text/javascript'>window.location = 'index.html'</script>";
-  }
+}
+
+
+// Use a function to check for email injection
+function IsInjected ($str)
+{
+    // Account for common injection techniques
+    $injCheck = array('(\n+)', '(\r+)', '(\t+)', '(%0A+)', '(%0D+)', '(%08+)', '(%09+)');
+    $inject = join('|', $injCheck);
+    $inject = "/$inject/i";
+
+    // Perform comparison
+    if (preg_match($inject, $str))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 ?>
